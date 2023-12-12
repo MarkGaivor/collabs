0124a0bac838d6f39f4e70f69a779f4aa4585640
MarkGaivor <153641030+MarkGaivor@users.noreply.github.com>
2023-12-12 19:35:38 +0300

    Update lab2task2.py

--- lab2task2.py
+++ lab2task2.py
@@ -1,9 +1,9 @@
-
-zpshka=237821
-traty=2123942
+zpshka=20000
+traty=25000
 capitalchik=traty-zpshka
 inflya=0.03
 for i in range(2,10):
-    zpshka*=(1+inflya)
-    capitalchik = traty - zpshka
-print(round(capitalchik+1))
+    traty*=(1+inflya)
+    capitalchik += traty - zpshka
+print(round(capitalchik,2))
+
