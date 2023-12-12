bc66a3c902812bc3ef68f01a3bd3275702097692
MarkGaivor <153641030+MarkGaivor@users.noreply.github.com>
2023-12-12 19:39:16 +0300

    Update lab2task2.py

--- lab2task2.py
+++ lab2task2.py
@@ -2,7 +2,7 @@ zpshka=20000
 traty=25000
 capitalchik=traty-zpshka
 inflya=0.03
-for i in range(2,10):
+for i in range(1,10):
     traty*=(1+inflya)
     capitalchik += traty - zpshka
 print(round(capitalchik,2))
